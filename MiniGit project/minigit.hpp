2988835e6c745032ec36cdce572d19eabb7780da
MisganaAB <misganatogod@gmail.com>
2025-06-17 17:49:01 +0300

    Update minigit.hpp
    minigit version control, added functions add/remove files, commiting changes , checkout by commit number, branch support, history tracking, and for clean up it includes destructors.

--- MiniGit project/minigit.hpp
+++ MiniGit project/minigit.hpp
@@ -4,6 +4,42 @@
 #include <string>
 using namespace std;
 
+struct FileNode {
+    string fileName;
+    string versionedFileName;
+    FileNode* next;
+};
+
+struct CommitNode {
+    int commitNumber;
+    FileNode* fileHead;
+    CommitNode* next;
+};
+
+class MiniGit {
+private:
+    CommitNode* commitHead;
+    int currentCommitNumber;
+    unordered_map<string, CommitNode*> branches;
+    
+    string currentBranch
+    int currentCommitNumber;
+
+public:
+    MiniGit();
+    ~MiniGit();
+
+    void addFile(const string& filename);
+    void removeFile(const string& filename);
+    void commit();
+    void checkout(int commitNumber);
+    
+    void printHistory();
+    void pringBranches();
+    
+    void createBranch(const string& name);
+    void checkoutBranch(const string& name);
+};
 
 
 #endif // MINIGIT_HPP_INCLUDED
