eaa0ea324ff0a2fd0ef16274ca7c38ab8f180eba
Nola8 <107250867+Nola8@users.noreply.github.com>
2025-06-20 07:15:07 +0300

    Update main.cpp

--- MiniGit project/main.cpp
+++ MiniGit project/main.cpp
@@ -1,57 +1,58 @@
 #include "minigit.hpp"
 #include <iostream>
+#include <vector>
+#include <string>
+
 using namespace std;
 
-int main() {
-    MiniGit git;
-    int choice;
-    string input;
-
-    while (true) {
-        cout << "\n===== MiniGit Menu =====\n";
-        cout << "1. Add File\n2. Remove File\n3. Commit Changes\n4. Checkout Commit\n";
-        cout << "5. View History\n6. Create Branch\n7. Switch Branch\n8. List Branches\n9. Exit\nSelect: ";
-        cin >> choice;
-        switch (choice) {
-            case 1:
-                cout << "File to add: "; 
-                cin >> input; 
-                git.addFile(input); 
-                break;
-            case 2:
-                cout << "File to remove: "; 
-                cin >> input; 
-                git.removeFile(input); 
-                break;
-            case 3:
-                git.commit(); 
-                break;
-            case 4:
-                int commitNum;
-                cout << "Commit # to checkout: "; 
-                cin >> commitNum; 
-                git.checkout(commitNum); 
-                break;
-            case 5:
-                git.printHistory(); 
-                break;
-            case 6:
-                cout << "New branch name: "; 
-                cin >> input; 
-                git.createBranch(input); 
-                break;
-            case 7:
-                cout << "Branch to switch: "; 
-                cin >> input; 
-                git.checkoutBranch(input); 
-                break;
-            case 8:
-                git.printBranches(); 
-                break;
-            case 9:
-                return 0;
-            default:
-                cout << "Invalid option.\n";
-        }
+int main(int argc, char* argv[]) {
+    std::vector<std::string> args;
+    for (int i = 1; i < argc; ++i) {
+        args.push_back(argv[i]);
     }
+    if (args.empty()) {
+        cout << "Usage:\n"
+             << "  add <filename>\n"
+             << "  remove <filename>\n"
+             << "  commit\n"
+             << "  checkout <commit#>\n"
+             << "  history\n"
+             << "  branch <branchname>\n"
+             << "  switch <branchname>\n"
+             << "  branches\n"
+             << "  diff <commit#1> <commit#2>\n";
+        return 1;
+    }
+    MiniGit git;
+    std::string cmd = args[0];
+    if (cmd == "init") {
+        git.init();
+    } else if (cmd == "add" && args.size() >= 2) {
+        git.addFile(args[1]);
+    } else if (cmd == "remove" && args.size() >= 2) {
+        git.removeFile(args[1]);
+    } else if (cmd == "commit") {
+        if (args.size() >= 3 && args[1] == "-m") {
+            git.commit(args[2]);
+        } else {
+            cout << "Usage: commit -m <message>\n";
+            return 1;
+        }
+    } else if (cmd == "checkout" && args.size() >= 2) {
+        git.checkout(stoi(args[1]));
+    } else if (cmd == "history") {
+        git.printHistory();
+    } else if (cmd == "branch" && args.size() >= 2) {
+        git.createBranch(args[1]);
+    } else if (cmd == "switch" && args.size() >= 2) {
+        git.checkoutBranch(args[1]);
+    } else if (cmd == "branches") {
+        git.printBranches();
+    } else if (cmd == "diff" && args.size() >= 3) {
+        git.diffCommits(stoi(args[1]), stoi(args[2]));
+    } else {
+        cout << "Invalid command or missing argument.\n";
+        return 1;
+    }
+    return 0;
 }
