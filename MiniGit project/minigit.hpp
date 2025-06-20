22ed68f4ae68d00eda20b9fb761e166e46bee54f
MisganaAB <misganatogod@gmail.com>
2025-06-20 22:22:17 +0300

    Update minigit.hpp
    included utils.hpp and added save and load function and made the checkout to use a branchName as an argument

--- MiniGit project/minigit.hpp
+++ MiniGit project/minigit.hpp
@@ -3,6 +3,8 @@
 
 #include <unordered_map>
 #include <string>
+#include "utils.hpp"
+
 using namespace std;
 
 struct FileNode {
@@ -24,21 +26,24 @@ private:
     CommitNode* commitHead;
     int currentCommitNumber;
     unordered_map<string, CommitNode*> branches;
-    
     string currentBranch;
+
+    void save();
+    void load();
 
 public:
     MiniGit();
 
     void mergeBranch(const string& branchName);
     void diffCommits(int commit1, int commit2);
+    void init();
 
     ~MiniGit();
 
     void addFile(const string& filename);
     void removeFile(const string& filename);
-    void commit();
-    void checkout(int commitNumber);
+    void commit(const string& message);
+    void checkout(const string& branchName);
     
     void printHistory();
     void printBranches();
